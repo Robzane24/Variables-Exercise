f9dc7864cba405d0b1f2e38a5dd83c7aad98c4c3
Robert Zane <rob.zane24@gmail.com>
2022-11-17 15:00:14 -0600

    Completed VariablesExercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string name = "Robbie";
+int age = 30;
+char middleInitial = 'W';
+bool isOver18 = true;
+double currentTemp = 70;
+decimal currentPrice = 2.9m;
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
